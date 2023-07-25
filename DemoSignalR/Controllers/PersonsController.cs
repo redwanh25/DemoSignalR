07cb59aaf1f1e5ce7f0a0fc993b12688d55c9809
Redwan Hossain <redwan.hossain@metlife.com.bd>
2023-07-25 14:01:40 +0600

    Add project files.

---
+++ DemoSignalR/Controllers/PersonsController.cs
@@ -0,0 +1,32 @@
+﻿using DemoSignalR.Hubs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace DemoSignalR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        private readonly List<string> list = new() { "Rahim", "Karim" };
+        private readonly IHubContext<NotificationHub> hubContext;
+
+        public PersonsController(IHubContext<NotificationHub> context)
+        {
+            hubContext = context;
+        }
+
+        [HttpGet("Get1")]
+        public async Task Get1Async()
+        {
+            await hubContext.Clients.All.SendAsync("MessageReceived", "Redwan");
+        }
+
+        [HttpGet("Get2")]
+        public List<string> Get2()
+        {
+            return list;
+        }
+    }
+}
---
+++ DemoSignalR/Hubs/NotificationHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DemoSignalR.Hubs
+{
+    public class NotificationHub : Hub
+    {
+        public async Task NewMessage(Message msg)
+        {
+            await Clients.All.SendAsync("MessageReceived", msg);
+        }
+    }
+
+    public class Message
+    {
+        public string ClientId { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
